57994900ec20343164acfa44254f76348706403b
dfzh <zzh@example.com>
2020-09-07 01:52:40 +0800

    gateway&es6

--- mall-product/src/main/java/com/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/mall/product/MallProductApplication.java
@@ -1,11 +1,11 @@
 package com.mall.product;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@MapperScan("com.mall.product.dao")
+@EnableDiscoveryClient
 public class MallProductApplication {
 
     public static void main(String[] args) {
